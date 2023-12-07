226aac19ee4b298364fce2d6d697cfbf95e2982b
Anton949432 <antonbratcikov@gmail.com>
2023-12-07 15:10:16 +0200

    аа

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import './App.css';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []); 
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (contact) => {
+    setContacts((prevContacts) => [...prevContacts, contact]);
+  };
+
+  const onDeleteContact = (contactId) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== contactId));
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Телефонна книга</h1>
+      <ContactForm contacts={contacts} addContact={addContact} />
+
+      <h2>Контакти</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={filteredContacts} onDeleteContact={onDeleteContact} />
     </div>
   );
 };
+
+export default App;
+
